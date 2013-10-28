e0d6a588d2fb82633674e3f52909ab1fd5db4907
StarvingMathematician <Macbook@new-host-2.home>
2013-10-28 18:21:51 -0400

    Finished designing and running the solution code
    The code was ran a few times, on the same set of dominoes using
    different SA parameters. Two different solutions were found, denoted by
    a suffixed "1" or "2".
    
    "domino_solver.py" -- contains classes and fuctions for solving the
    Domino puzzle levels from the Android app 'Mind Games'
    "tester2.py" -- imports and runs 'domino_solver' to actually solve the
    puzzles
    "Solution_Output_.txt" -- what was printed to the screen when the code
    had finished running
    "Successful_Level2_.txt" -- a graph of the progress of the SA algorithm
    "best_list_.txt" -- a text file containing the cost of the best domino
    configuration found at that point in the algorithm
    "cost_list_.txt" -- a text file containing the cost of the domino
    configuration at every step of the algorithm

--- domino_solver.py
+++ domino_solver.py
@@ -8,6 +8,7 @@ https://gist.github.com/ryangomba/1724881
 '''
 
 import random
+random.seed()
 
 class Domino:
     '''
@@ -20,7 +21,7 @@ class Domino:
     There is also a flip operation which swaps the domino's two numbers  
     '''
     
-    def __init__(self, bottom_num, top_num):
+    def __init__(self, top_num, bottom_num):
         self.top = top_num
         self.bottom = bottom_num
         self.total = top_num + bottom_num
@@ -47,7 +48,6 @@ class Domino_Board:
     def _get_domino_layout(self, num_pair_list):
         domino_obj_list = [Domino(*num_pair) for num_pair in num_pair_list]
         
-        random.seed()
         random.shuffle(domino_obj_list)
         for domino in domino_obj_list:
             if random.random() > .5:
@@ -57,6 +57,10 @@ class Domino_Board:
         domino_layout = [domino_obj_list[self.board_width*i:self.board_width*(i+1)] for i in range(self.board_height)]
         return domino_layout
     
+    def _get_printable_domino_layout(self):
+        return [[[self.domino_layout[row][col].top, self.domino_layout[row][col].bottom] for col in range(self.board_width)] for row in range(self.board_height)]
+                
+        
     #Swaps a pair of dominoes on the board
     def _swap_dominos(self, idx1, idx2):
         self.domino_layout[idx1[0]][idx1[1]], self.domino_layout[idx2[0]][idx2[1]] = self.domino_layout[idx2[0]][idx2[1]], self.domino_layout[idx1[0]][idx1[1]]
@@ -69,20 +73,16 @@ class Domino_Board:
             self.domino_layout[idx[0]][idx[1]]._flip()
             return True
     
-    #Consider flip symmetry to be irrelevant. If we perform a trivial flip, then so be it.
-    #def _flip_domino(self,idx):
-    #    self.domino_layout[idx[0]][idx[1]]._flip()
-        
     #Numbering is top to bottom: 'domino_row' goes up to 'board_height', but 'digit_row' can only be 0 or 1 (for 'top' or 'bottom')
     def _row_sum(self, domino_row, digit_row):
         if digit_row == 0:
-            return sum([self.domino_layout[domino_row][col].top for col in self.board_width])
+            return sum([self.domino_layout[domino_row][col].top for col in range(self.board_width)])
         elif digit_row == 1:
-            return sum([self.domino_layout[domino_row][col].bottom for col in self.board_width])
+            return sum([self.domino_layout[domino_row][col].bottom for col in range(self.board_width)])
     
     #Numbering is left to right
-    def _column_sum(self, col_idx):
-        return sum([self.domino_layout[row][col_idx].total for row in self.board_height])
+    def _col_sum(self, col_idx):
+        return sum([self.domino_layout[row][col_idx].total for row in range(self.board_height)])
     
     #diag_num == 0 --> main diag, diag_num == 1 --> other diag 
     def _diag_sum(self, diag_num):
@@ -94,7 +94,7 @@ class Domino_Board:
     def _get_cost(self):
         col_cost = sum([abs(self.magic_number - self._col_sum(i)) for i in range(self.board_width)]) 
         row_cost = sum([abs(self.magic_number - self._row_sum(i,j)) for j in [0,1] for i in range(self.board_height)])
-        diag_cost = abs(self.magic_number - self.diag_sum(0)) + abs(self.magic_number - self.diag_sum(1))
+        diag_cost = abs(self.magic_number - self._diag_sum(0)) + abs(self.magic_number - self._diag_sum(1))
         return col_cost + row_cost + diag_cost
     
     
@@ -161,37 +161,67 @@ class Domino_Solver:
     #The probability of swap vs flip should correspond to the number of unique ways in which they can occur.
     #That is, swap is proportional to '(size choose 2)', and flip is proportional to 'size'
     #Thus, we choose a random number in the range [0, size + (size choose 2)], and swap if it's > n, and flip otherwise  
-    def _get_neighboring_state(self):
-        random.seed()
+    def _get_neighboring_state(self, This_Board):
         if random.uniform(0, self.board_size + (self.board_size * (self.board_size-1)) / 2) > self.board_size: #perform swap
             idx1 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
             idx2 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
             while idx1 == idx2:
                 idx2 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
-            self.This_Board._swap_dominos(idx1, idx2)
+            This_Board._swap_dominos(idx1, idx2)
         else:
             idx = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
-            while not self.This_Board._flip_domino(idx): #recall that if the domino is flip symmetric, then 'flip_domino' simply returns 'False' 
+            while not This_Board._flip_domino(idx): #recall that if the domino is flip symmetric, then 'flip_domino' simply returns 'False' 
                 idx = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
         
-    def _get_init_temp(self, num_steps):
-        
+    def _get_init_temp(self, This_Board, num_steps):
+        from numpy import zeros, std
+        cost_arr = zeros(num_steps)
+        for n in range(num_steps):
+            cost_arr[n] = This_Board._get_cost()
+            self._get_neighboring_state(This_Board)
+        return std(cost_arr)
 
     ##########################################################################################
             
     def run_SA(self, num_pair_list):
-        self.This_Board = Domino_Board(self.board_height, self.board_width, self.magic_number, num_pair_list)
-        cooling_rate = .99
+        from copy import deepcopy
+        from math import exp
+        
+        This_Board = Domino_Board(self.board_height, self.board_width, self.magic_number, num_pair_list)
+        cooling_rate = .98 # <-- solution parameter value
         mdp_length = self.board_size ** 2 #consider changing this value to something more meaningful
-        temp = self._get_init_temp(mdp_length) #I can choose this value more intelligently later, if I want
+        temp = self._get_init_temp(This_Board, mdp_length) #I can choose this value more intelligently later, if I want
         
+        best_cost = curr_cost = This_Board._get_cost()
+        best_layout = deepcopy(This_Board.domino_layout)
+        best_list = [[0, best_cost]]
         
-    
-    
-    
-    
-    
-    
-    
-    
+        max_steps = mdp_length*200 # <-- solution parameter value
+        cost_list = [curr_cost]
+        step_num = 0
+        
+        #no reheating
+        while best_cost > 0 and step_num < max_steps:
+            step_num += 1
+            Prev_Board = deepcopy(This_Board)
+            self._get_neighboring_state(This_Board)
+            curr_cost = This_Board._get_cost()
+            cost_diff = curr_cost - Prev_Board._get_cost()
+            
+            if cost_diff < 0 or random.random() < exp(-cost_diff / temp): #if the cost of the new board is better, or tunneling occurs
+                if curr_cost < best_cost:
+                    best_cost = curr_cost
+                    best_layout = This_Board._get_printable_domino_layout()
+                    best_list.append([step_num,curr_cost])
+            else:
+                This_Board = Prev_Board
+            
+            cost_list.append(curr_cost)
+            if step_num % mdp_length == 0:
+                temp *= cooling_rate
+                if (step_num / mdp_length) % 10 == 0:
+                    print "Done with loop number", step_num / mdp_length
+        
+        return best_cost, best_layout, cost_list, best_list
+ 
     
--- tester2.py
+++ tester2.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     print timeElapsed
     '''
     
+    '''
     #2.2sec
     Level_2 = Domino_Solver(3, 6, 13) #board height, board width, magic number
     
@@ -38,3 +39,44 @@ if __name__ == '__main__':
     #Corresponding to the sum list [7 7 7 6 6 6 6 5 5 4 4 4 3 3 2 2 1 0] (skipping [0,5]):
     domino_list = [[1,6],[2,5],[3,4],[0,6],[1,5],[2,4],[3,3],[1,4],[2,3],[0,4],[1,3],[2,2],[0,3],[1,2],[0,2],[1,1],[0,1],[0,0]]
     min_solution = Level_2.run_SA(domino_list)
+    '''
+    
+    # Takes .2sec simply to initialize the problem. Not a good sign...
+    Level_2 = Domino_Solver(3, 6, 13) #board height, board width, magic number
+    
+    #Corresponding to the sum list [7 7 7 6 6 6 6 5 5 4 4 4 3 3 2 2 1 0] (skipping [0,5]):
+    domino_list = [[1,6],[2,5],[3,4],[0,6],[1,5],[2,4],[3,3],[1,4],[2,3],[0,4],[1,3],[2,2],[0,3],[1,2],[0,2],[1,1],[0,1],[0,0]]
+    startTime = time()
+    best_cost, best_layout, cost_list, best_list = Level_2.run_SA(domino_list) #min_solution = Level_2.run_SA(domino_list)
+    timeElapsed = time()-startTime
+    print "Time Elapsed:", timeElapsed
+    
+    print "Best Cost:", best_cost
+    print "Best Layout:"
+    for row in best_layout:
+        print row
+    
+    filename1 = "/Users/Macbook/Documents/Git_Repos/Mind-Games-domino-solver/cost_list.txt"
+    f1 = open(filename1, 'w')
+    for i1, cost in enumerate(cost_list):
+        f1.write("%s," % i1)
+        f1.write("%s\n" % cost)
+    f1.close()
+    
+    filename2 = "/Users/Macbook/Documents/Git_Repos/Mind-Games-domino-solver/best_list.txt"
+    f2 = open(filename2, 'w')
+    for i2, best in best_list:
+        f2.write("%s," % i2)
+        f2.write("%s\n" % best)
+    f2.close()
+  
+    import matplotlib.pyplot as plt
+    best_idx, best_val = zip(*best_list)
+    plt.plot(cost_list, 'b', best_idx, best_val, 'r')
+    plt.show()
+    
+    
+    
+    
+    
+    
