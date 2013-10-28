774f5bf8721602ab6fc9733bd864d97537caaafa
StarvingMathematician <Macbook@new-host-2.home>
2013-10-28 10:39:16 -0400

    Added the Domino_Boards class, and began work on the SA paradigm

--- domino_solver.py
+++ domino_solver.py
@@ -7,6 +7,8 @@ Structure is based on the code found here:
 https://gist.github.com/ryangomba/1724881 
 '''
 
+import random
+
 class Domino:
     '''
     Each domino is represented by 4 parameters:
@@ -18,17 +20,82 @@ class Domino:
     There is also a flip operation which swaps the domino's two numbers  
     '''
     
-    def __init__(self, lower_num, upper_num):
-        self.lower_num = lower_num
-        self.upper_num = upper_num
-        self.total_num = lower_num + upper_num
-        self.flip_sym = lower_num == upper_num
+    def __init__(self, bottom_num, top_num):
+        self.top = top_num
+        self.bottom = bottom_num
+        self.total = top_num + bottom_num
+        self.flip_sym = top_num == bottom_num
         
     def _flip(self):
-        self.lower_num, self.upper_num = self.upper_num, self.lower_num        
+        self.bottom, self.top = self.top, self.bottom        
                 
                 
     ##########################################################################################
+
+
+#Domino_Board shares attributes (width/height) with Domino_Solver. This isn't ideal, but I'll leave it for now. 
+class Domino_Board:
+    '''
+    Denotes the board_heightxboard_width board of dominoes which sum to magic_number
+    '''
+    def __init__(self, board_height, board_width, magic_number, num_pair_list):
+        self.board_height = board_height #e.g. 3
+        self.board_width = board_width # e.g. 6
+        self.magic_number = magic_number # e.g. 13
+        self.domino_layout = self._get_domino_layout(num_pair_list)
+    
+    def _get_domino_layout(self, num_pair_list):
+        domino_obj_list = [Domino(*num_pair) for num_pair in num_pair_list]
+        
+        random.seed()
+        random.shuffle(domino_obj_list)
+        for domino in domino_obj_list:
+            if random.random() > .5:
+                domino._flip()
+        
+        #Reshape the board
+        domino_layout = [domino_obj_list[self.board_width*i:self.board_width*(i+1)] for i in range(self.board_height)]
+        return domino_layout
+    
+    #Swaps a pair of dominoes on the board
+    def _swap_dominos(self, idx1, idx2):
+        self.domino_layout[idx1[0]][idx1[1]], self.domino_layout[idx2[0]][idx2[1]] = self.domino_layout[idx2[0]][idx2[1]], self.domino_layout[idx1[0]][idx1[1]]
+    
+    #Returns 'True' and flips a domino on the board if it's *not* flip symmetric, otherwise simply return 'False'
+    def _flip_domino(self,idx):
+        if self.domino_layout[idx[0]][idx[1]].flip_sym == True:
+            return False
+        else:
+            self.domino_layout[idx[0]][idx[1]]._flip()
+            return True
+    
+    #Consider flip symmetry to be irrelevant. If we perform a trivial flip, then so be it.
+    #def _flip_domino(self,idx):
+    #    self.domino_layout[idx[0]][idx[1]]._flip()
+        
+    #Numbering is top to bottom: 'domino_row' goes up to 'board_height', but 'digit_row' can only be 0 or 1 (for 'top' or 'bottom')
+    def _row_sum(self, domino_row, digit_row):
+        if digit_row == 0:
+            return sum([self.domino_layout[domino_row][col].top for col in self.board_width])
+        elif digit_row == 1:
+            return sum([self.domino_layout[domino_row][col].bottom for col in self.board_width])
+    
+    #Numbering is left to right
+    def _column_sum(self, col_idx):
+        return sum([self.domino_layout[row][col_idx].total for row in self.board_height])
+    
+    #diag_num == 0 --> main diag, diag_num == 1 --> other diag 
+    def _diag_sum(self, diag_num):
+        if diag_num == 0:
+            return sum([self.domino_layout[i/2][i].top if i%2==0 else self.domino_layout[(i-1)/2][i].bottom for i in range(self.board_width)])
+        elif diag_num == 1:
+            return sum([self.domino_layout[self.board_height-1 - i/2][i].bottom if i%2==0 else self.domino_layout[self.board_height-1 - (i-1)/2][i].top for i in range(self.board_width)])
+    
+    def _get_cost(self):
+        col_cost = sum([abs(self.magic_number - self._col_sum(i)) for i in range(self.board_width)]) 
+        row_cost = sum([abs(self.magic_number - self._row_sum(i,j)) for j in [0,1] for i in range(self.board_height)])
+        diag_cost = abs(self.magic_number - self.diag_sum(0)) + abs(self.magic_number - self.diag_sum(1))
+        return col_cost + row_cost + diag_cost
     
     
 class Domino_Solver:
@@ -38,14 +105,8 @@ class Domino_Solver:
         self.board_width = board_width # e.g. 6
         self.board_size = board_height*board_width # e.g. 18
         self.magic_number = magic_number # e.g. 13
-    '''    
-    def _get_all_dominos(self):
-        all_dominos = [Domino(i,j) for i in range(7) for j in range(i+1)]
-        return all_dominos
-    '''
+    
     ##########################################################################################
-    
-    
     
     def _generate_all_sums(self):
         all_sums = []
@@ -58,10 +119,10 @@ class Domino_Solver:
     
     def _generate_unique_subsets(self,grouped_sums):
         
-        def _recursive_generate(amount_remaining, dominos_remaining, grouped_sums_remaining, this_subset):
-            if dominos_remaining < 0:
+        def _recursive_generate(amount_remaining, dominoes_remaining, grouped_sums_remaining, this_subset):
+            if dominoes_remaining < 0:
                 return []
-            elif dominos_remaining == 0:
+            elif dominoes_remaining == 0:
                 if amount_remaining == 0:
                     return this_subset
                 else:
@@ -73,11 +134,11 @@ class Domino_Solver:
                     # Only difference between this cases is whether we reduce the front element freq by 1 (which requires making a deep copy), or remove it altogether.
                     # Note that reducing the frequency only makes sense to do if the freq is >1
                     if grouped_sums_remaining[0][1] == 1:
-                        return _recursive_generate(amount_remaining - grouped_sums_remaining[0][0], dominos_remaining - 1, grouped_sums_remaining[1:], this_subset + [grouped_sums_remaining[0][0]]) + _recursive_generate(amount_remaining, dominos_remaining, grouped_sums_remaining[1:], this_subset[:])
+                        return _recursive_generate(amount_remaining - grouped_sums_remaining[0][0], dominoes_remaining - 1, grouped_sums_remaining[1:], this_subset + [grouped_sums_remaining[0][0]]) + _recursive_generate(amount_remaining, dominoes_remaining, grouped_sums_remaining[1:], this_subset[:])
                     else:
                         head_copy = grouped_sums_remaining[0][:]
                         head_copy[1] -= 1
-                        return _recursive_generate(amount_remaining - grouped_sums_remaining[0][0], dominos_remaining - 1, [head_copy] + grouped_sums_remaining[1:], this_subset + [grouped_sums_remaining[0][0]]) + _recursive_generate(amount_remaining, dominos_remaining, grouped_sums_remaining[1:], this_subset[:])
+                        return _recursive_generate(amount_remaining - grouped_sums_remaining[0][0], dominoes_remaining - 1, [head_copy] + grouped_sums_remaining[1:], this_subset + [grouped_sums_remaining[0][0]]) + _recursive_generate(amount_remaining, dominoes_remaining, grouped_sums_remaining[1:], this_subset[:])
         
         solution_blob = _recursive_generate(self.magic_number*self.board_width, self.board_size, grouped_sums, [])
         #Need to reshape solution list before returning it 
@@ -92,13 +153,40 @@ class Domino_Solver:
         
         return unique_subsets
         
+    ##########################################################################################
     
+    #First decide whether we're going to flip or swap, and then choose random domino(es) to do it to
+    #I will probably NOT want add a 'trade' option, since that undermines the nature of the Domino_Board object
+    #
+    #The probability of swap vs flip should correspond to the number of unique ways in which they can occur.
+    #That is, swap is proportional to '(size choose 2)', and flip is proportional to 'size'
+    #Thus, we choose a random number in the range [0, size + (size choose 2)], and swap if it's > n, and flip otherwise  
+    def _get_neighboring_state(self):
+        random.seed()
+        if random.uniform(0, self.board_size + (self.board_size * (self.board_size-1)) / 2) > self.board_size: #perform swap
+            idx1 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
+            idx2 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
+            while idx1 == idx2:
+                idx2 = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
+            self.This_Board._swap_dominos(idx1, idx2)
+        else:
+            idx = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
+            while not self.This_Board._flip_domino(idx): #recall that if the domino is flip symmetric, then 'flip_domino' simply returns 'False' 
+                idx = [random.randint(0,self.board_height - 1),random.randint(0,self.board_width - 1)]
+        
+    def _get_init_temp(self, num_steps):
+        
+
+    ##########################################################################################
+            
+    def run_SA(self, num_pair_list):
+        self.This_Board = Domino_Board(self.board_height, self.board_width, self.magic_number, num_pair_list)
+        cooling_rate = .99
+        mdp_length = self.board_size ** 2 #consider changing this value to something more meaningful
+        temp = self._get_init_temp(mdp_length) #I can choose this value more intelligently later, if I want
+        
+        
     
-    
-    '''
-    To save on space and time, only generate sums, not the dominos.
-    We can generate a list of domino objects when they're needed.
-    '''
     
     
     
--- tester2.py
+++ tester2.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     
     '''
     #.017sec
-    level1 = Domino_Solver(2, 4, 6) #board height, board width, magic number
+    Level_1 = Domino_Solver(2, 4, 6) #board height, board width, magic number
     
     startTime = time()
-    solution_set = level1.get_plausible_subsets()
+    solution_set = Level_1.get_plausible_subsets()
     timeElapsed = time()-startTime
     
     print solution_set
@@ -24,10 +24,10 @@ if __name__ == '__main__':
     '''
     
     #2.2sec
-    level2 = Domino_Solver(3, 6, 13) #board height, board width, magic number
+    Level_2 = Domino_Solver(3, 6, 13) #board height, board width, magic number
     
     startTime = time()
-    solution_set = level2.get_plausible_subsets()
+    solution_set = Level_2.get_plausible_subsets()
     timeElapsed = time()-startTime
     
     for sol in solution_set:
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     
     #Corresponding to the sum list [7 7 7 6 6 6 6 5 5 4 4 4 3 3 2 2 1 0] (skipping [0,5]):
     domino_list = [[1,6],[2,5],[3,4],[0,6],[1,5],[2,4],[3,3],[1,4],[2,3],[0,4],[1,3],[2,2],[0,3],[1,2],[0,2],[1,1],[0,1],[0,0]]
-    min_solution = level2.run_SA(domino_list)
+    min_solution = Level_2.run_SA(domino_list)
